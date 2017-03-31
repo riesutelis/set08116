d1fc588d03546f939c31555199e52dcbb6e036c0
Paulius Bieksa <priverstinis@gmail.com>
2017-03-31 11:13:18 +0100

    Up to and including 58.

--- labs/practicals/57_Skybox/57_Skybox.cpp
+++ labs/practicals/57_Skybox/57_Skybox.cpp
@@ -17,14 +17,15 @@ bool load_content() {
   sphere = mesh(geometry_builder::create_sphere(25, 25));
   // *********************************
   // Create box geometry for skybox
-
+  skybox = mesh(geometry_builder::create_box());
   // Scale box by 100
-
+  skybox.get_transform().scale = vec3(-100, -100, -100);
+  skybox.get_transform().rotate(rotate(mat4(1), pi<float>(), vec3(0.0f, 0.0f, 1.0f)));
   // Load the cubemap
-
-
+  array<string, 6> filenames = { "textures/sahara_ft.jpg", "textures/sahara_bk.jpg", "textures/sahara_up.jpg",
+	  "textures/sahara_dn.jpg", "textures/sahara_rt.jpg", "textures/sahara_lf.jpg" };
   // Create cube_map
-
+  cube_map = cubemap(filenames);
   // *********************************
   // Load in shaders
   eff.add_shader("57_Skybox/shader.vert", GL_VERTEX_SHADER);
@@ -34,10 +35,10 @@ bool load_content() {
 
   // *********************************
   // Load in skybox effect
-
-
+  sky_eff.add_shader("shaders/skybox.vert", GL_VERTEX_SHADER);
+  sky_eff.add_shader("shaders/skybox.frag", GL_FRAGMENT_SHADER);
   // Build effect
-
+  sky_eff.build();
   // *********************************
 
   // Set camera properties
@@ -53,7 +54,7 @@ bool update(float delta_time) {
   cam.update(delta_time);
   // *********************************
   // Set skybox position to camera position (camera in centre of skybox)
-
+  skybox.get_transform().position = cam.get_position();
   // *********************************
   return true;
 }
@@ -61,26 +62,26 @@ bool update(float delta_time) {
 bool render() {
   // *********************************
   // Disable depth test,depth mask,face culling
-
-
+	glDisable(GL_DEPTH_TEST);
+	glDepthMask(GL_FALSE);
 
   // Bind skybox effect
-
+	renderer::bind(sky_eff);
   // Calculate MVP for the skybox
-
-
-
-
+	mat4 M = skybox.get_transform().get_transform_matrix();
+	mat4 V = cam.get_view();
+	mat4 P = cam.get_projection();
+	mat4 MVP = P * V * M;
   // Set MVP matrix uniform
-
+	glUniformMatrix4fv(sky_eff.get_uniform_location("MVP"), 1, GL_FALSE, value_ptr(MVP));
   // Set cubemap uniform
-
-
+	renderer::bind(cube_map, 0);
+	glUniform1i(sky_eff.get_uniform_location("cubemap"), 0);
   // Render skybox
-
+	renderer::render(skybox);
   // Enable depth test,depth mask,face culling
-
-
+	glEnable(GL_DEPTH_TEST);
+	glDepthMask(GL_TRUE);
 
   // *********************************
 
